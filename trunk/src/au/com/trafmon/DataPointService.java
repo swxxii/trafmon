74938c9c2dc9967a06d2a31c61e2bf66a6076931
Chester.Nut.87 <Chester.Nut.87@65cdba2e-a324-11de-a118-c7387b1e4f66>
2009-10-26 11:22:24 +0000

    Added in second method to get points during a specific minute on a specific date. Most likely we will want something of a compromise between these two in the end

--- trunk/src/au/com/trafmon/DataPointService.java
+++ trunk/src/au/com/trafmon/DataPointService.java
@@ -11,6 +11,16 @@ import com.db4o.query.Predicate;
 
 public class DataPointService {
 	
+	/**
+	 * This method returns all points on a given day of the week during a given hour
+	 * within the given bounds
+	 * @param maxLat
+	 * @param maxLng
+	 * @param minLat
+	 * @param minLng
+	 * @param date
+	 * @return
+	 */
 	public static DataPointSet getPointsByDayOfWeek(final Double maxLat, final Double maxLng, final Double minLat, final Double minLng, Date date){
 
 		TimeZone tz = TimeZone.getDefault();
@@ -34,6 +44,60 @@ public class DataPointService {
 					  && candidate.getLng() >= minLng
 					  && curCal.get(Calendar.DAY_OF_WEEK) == requestTime.get(Calendar.DAY_OF_WEEK)
 					  && curCal.get(Calendar.HOUR_OF_DAY) == requestTime.get(Calendar.HOUR_OF_DAY)
+					   ) {
+
+						return true;
+					} else {
+						return false;
+					}
+				}
+			});
+
+			DataPointSet pointsSet = new DataPointSet();
+
+			pointsSet.setDataPoints(dataPoints);
+
+			return pointsSet;
+		} finally {
+			db.close();
+		}
+	}
+	
+	/**
+	 * This method returns all points on a given date during a given minute
+	 * within the given bounds
+	 * @param maxLat
+	 * @param maxLng
+	 * @param minLat
+	 * @param minLng
+	 * @param date
+	 * @return
+	 */
+	public static DataPointSet getPointsByDate(final Double maxLat, final Double maxLng, final Double minLat, final Double minLng, Date date){
+
+		TimeZone tz = TimeZone.getDefault();
+
+		final GregorianCalendar requestTime = new GregorianCalendar(tz);
+
+		requestTime.setTime(date);
+
+		ObjectContainer db = Util.openDb();
+
+		try {
+			List<DataPoint> dataPoints = db.query(new Predicate<DataPoint>() {
+				public boolean match(DataPoint candidate) {
+					GregorianCalendar curCal = candidate.getCal();
+					// Check we are within the bounds specified, and within the hour specified.
+					// Currently this will get all points, for example, that were made on a Tuesday at 4pm within the given bounds
+					// It will NOT get all points that, for example, were made on the 4th of July 2008 between 4pm and 4:05pm within the given bounds
+					if ( candidate.getLat() <= maxLat 
+					  && candidate.getLat() >= minLat 
+					  && candidate.getLng() <= maxLng 
+					  && candidate.getLng() >= minLng
+					  && curCal.get(Calendar.YEAR) == requestTime.get(Calendar.YEAR)
+					  && curCal.get(Calendar.DAY_OF_YEAR) == requestTime.get(Calendar.DAY_OF_YEAR)
+					  && curCal.get(Calendar.HOUR_OF_DAY) == requestTime.get(Calendar.HOUR_OF_DAY)
+					  && curCal.get(Calendar.MINUTE) == requestTime.get(Calendar.MINUTE)
 					   ) {
 
 						return true;
